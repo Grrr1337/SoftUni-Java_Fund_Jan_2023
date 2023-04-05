20d7032783470e4526d7836b7fa88626d8cb4a0c
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-04-05 07:42:43 +0300

    Small changes on "Elevator.java"

--- src/DataTypesAndVariables/Elevator.java
+++ src/DataTypesAndVariables/Elevator.java
@@ -6,10 +6,10 @@ public class Elevator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.print("Enter the number of people: ");
+        System.out.print("Enter the number of people: ");
         int n = scanner.nextInt();
 
-//        System.out.print("Enter the capacity of the elevator: ");
+        System.out.print("Enter the capacity of the elevator: ");
         int p = scanner.nextInt();
 
         int fullCourses = n / p;
