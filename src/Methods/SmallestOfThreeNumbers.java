fe068cc2100c6af27b803458a6a45dd2a7fd8903
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-04-10 07:37:30 +0300

    Update SmallestOfThreeNumbers

--- src/Methods/SmallestOfThreeNumbers.java
+++ src/Methods/SmallestOfThreeNumbers.java
@@ -6,17 +6,17 @@ public class SmallestOfThreeNumbers {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-//        System.out.print("Enter the first number: ");
+        System.out.print("Enter the first number: ");
         int num1 = input.nextInt();
 
-//        System.out.print("Enter the second number: ");
+        System.out.print("Enter the second number: ");
         int num2 = input.nextInt();
 
-//        System.out.print("Enter the third number: ");
+        System.out.print("Enter the third number: ");
         int num3 = input.nextInt();
 
         int smallest = findSmallest(num1, num2, num3);
-//        System.out.println("The smallest number is: " + smallest);
+        System.out.println("The smallest number is: " + smallest);
         System.out.println(smallest);
         input.close();
     }
