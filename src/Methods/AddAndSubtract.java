29551bb065776a2ee4249ec5c61bf13570abd16b
Vladimir Balabanov <38167466+Grrr1337@users.noreply.github.com>
2023-04-11 07:39:47 +0300

    Update AddAndSubtract.java

--- src/Methods/AddAndSubtract.java
+++ src/Methods/AddAndSubtract.java
@@ -6,6 +6,7 @@ public class AddAndSubtract {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Input first integer: ");
         int num1 = scanner.nextInt();
         int num2 = scanner.nextInt();
         int num3 = scanner.nextInt();
